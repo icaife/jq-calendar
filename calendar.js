76496e516f730b8aeba51b3c542eb7848e0a783e
Leon Cai <leon.cai@toursforfun.com>
2015-07-03 17:36:10 +0800

    fix some bug.

--- calendar.js
+++ calendar.js
@@ -212,11 +212,11 @@ $(function() {
 				if (!_cfgs.showOutOfRange) {
 					var tmp = that.getDate(_cfgs[type === "next" ? "maxDate" : "minDate"]);
 					if (type === "next") {
-						if (_y + _m + _cfgs.showMonths > tmp.year + tmp.month) { //超过最大的日期
+						if (_y * 100 + _m + _cfgs.showMonths > tmp.year * 100 + tmp.month) { //超过最大的日期
 							return false;
 						}
 					} else { //小于最小的日期
-						if (_y + _m - _cfgs.showMonths < tmp.year + tmp.month) {
+						if (_y * 100 + _m - _cfgs.showMonths < tmp.year * 100 + tmp.month) {
 							return false;
 						}
 					}
@@ -723,13 +723,12 @@ $(function() {
 	var start = $(".cal-start")
 		.tffCal({
 			onSelect: function(dateStr, date) {
-				// console.log(arguments);
-				end.tffCal("changeOption", {
-					minDate: date
-				});
-				console.log("start date selected!");
-			},
-			minDate: new Date(2015, 5, 1)
+					// console.log(arguments);
+					end.tffCal("changeOption", {
+						minDate: date
+					});
+					console.log("start date selected!");
+				}
 				// selectedDate: new Date(2015, 5, 10)
 		});
 
@@ -745,43 +744,43 @@ $(function() {
 				// end.tffCal("changeOption", {
 				// 	minDate: data
 				// });
-				console.log("start date selected!");
-			},
-			minDate: new Date(2015, 5, 1)
+				console.log("end date selected!");
+			}
 		});
 
 	start.tffCal("relateTo", end); //start 和 end 关联起来
 
 });
 
-
-//                            _ooOoo_  
-//                           o8888888o  
-//                           88" . "88  
-//                           (| -_- |)  
-//                            O\ = /O  
-//                        ____/`---'\____  
-//                      .   ' \\| |// `.  
-//                       / \\||| : |||// \  
-//                     / _||||| -:- |||||- \  
-//                       | | \\\ - /// | |  
-//                     | \_| ''\---/'' | |  
-//                      \ .-\__ `-` ___/-. /  
-//                   ___`. .' /--.--\ `. . __  
-//                ."" '< `.___\_<|>_/___.' >'"".  
-//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
-//                 \ \ `-. \_ __\ /__ _/ .-` / /  
-//         ======`-.____`-.___\_____/___.-`____.-'======  
-//                            `=---='  
-//  
-//         .............................................  
-//                  佛祖保佑             永无BUG 
-//          佛曰:  
-//                  写字楼里写字间，写字间里程序员；  
-//                  程序人员写程序，又拿程序换酒钱。  
-//                  酒醒只在网上坐，酒醉还来网下眠；  
-//                  酒醉酒醒日复日，网上网下年复年。  
-//                  但愿老死电脑间，不愿鞠躬老板前；  
-//                  奔驰宝马贵者趣，公交自行程序员。  
-//                  别人笑我忒疯癫，我笑自己命太贱；  
-//                  不见满街漂亮妹，哪个归得程序员？
+/*
+                           _ooOoo_  
+                          o8888888o  
+                          88" . "88  
+                          (| -_- |)  
+                           O\ = /O  
+                       ____/`---'\____  
+                     .   ' \\| |// `.  
+                      / \\||| : |||// \  
+                    / _||||| -:- |||||- \  
+                      | | \\\ - /// | |  
+                    | \_| ''\---/'' | |  
+                     \ .-\__ `-` ___/-. /  
+                  ___`. .' /--.--\ `. . __  
+               ."" '< `.___\_<|>_/___.' >'"".  
+              | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+                \ \ `-. \_ __\ /__ _/ .-` / /  
+        ======`-.____`-.___\_____/___.-`____.-'======  
+                           `=---='  
+ 
+        .............................................  
+                 佛祖保佑             永无BUG 
+         佛曰:  
+                 写字楼里写字间，写字间里程序员；  
+                 程序人员写程序，又拿程序换酒钱。  
+                 酒醒只在网上坐，酒醉还来网下眠；  
+                 酒醉酒醒日复日，网上网下年复年。  
+                 但愿老死电脑间，不愿鞠躬老板前；  
+                 奔驰宝马贵者趣，公交自行程序员。  
+                 别人笑我忒疯癫，我笑自己命太贱；  
+                 不见满街漂亮妹，哪个归得程序员？ 
+ */
